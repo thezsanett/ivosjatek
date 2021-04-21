9f110f0cda7a3666bd9d6e758de2b837d30c9fdc
thezsanett <armbruszt.zsanett@icloud.com>
2021-04-21 08:10:38 +0200

    Ordering truth or drink questions.

--- src/getters/functions.js
+++ src/getters/functions.js
@@ -12,3 +12,17 @@ export function shuffle(array) {
 
     return array;
 }
+
+export function insertToArrayWithOffset(shuffledArray, dataArrayToInsert, randomOffset = false) {
+
+    dataArrayToInsert.forEach((tuple) => {
+        const randomPosition = Math.floor(Math.random() * shuffledArray.length);
+        shuffledArray.splice(randomPosition, 0, tuple[0]);
+
+        let offSet = 1;
+        if (randomOffset) offSet = Math.floor(Math.random() * (shuffledArray.length-randomPosition-1));
+        shuffledArray.splice(randomPosition+offSet, 0, tuple[1]);
+    });
+
+    return shuffledArray;
+}
--- src/getters/statements.js
+++ src/getters/statements.js
@@ -1,10 +1,10 @@
-import { questions } from "../data/truthDrinkData";
+import { questions, orderedQuestions, followingQuestions } from "../data/truthDrinkData";
 import { basicStatements, sexyStatements } from "../data/neverHaveData";
 import { choices } from "../data/votingGameData";
 import { commands } from "../data/kingsCupData";
 import { attributes } from "../data/drinkIfData";
 
-import { shuffle } from "./functions";
+import { shuffle, insertToArrayWithOffset } from "./functions";
 
 
 const votingData = shuffle(choices);
@@ -12,7 +12,10 @@ const kingsData = shuffle(commands);
 const ifData = shuffle(attributes);
 const neverData = shuffle(basicStatements);
 const neverData18 = shuffle(sexyStatements);
-const truthData = shuffle(questions);
+
+const shuffledTruthData = shuffle(questions);
+const orderedTruthData = insertToArrayWithOffset(shuffledTruthData, orderedQuestions, true);
+const truthData = insertToArrayWithOffset(orderedTruthData, followingQuestions, false);
 
 export { 
     votingData,
